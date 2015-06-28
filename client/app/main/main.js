bfcfa3b8224560551ab5ea55d2a1e3c557481d5c
mikeonslow <mikeonslow@gmaill.com>
2015-06-28 18:45:14 -0400

    Angular Fullstack Base

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('estimatorApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
